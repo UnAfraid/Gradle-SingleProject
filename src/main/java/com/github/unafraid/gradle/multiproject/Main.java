3d8a90e6593d5dba0337616bbaa67d7673355380
UnAfraid <unafraid89@gmail.com>
2018-04-09 20:59:39 +0300

    Initial version

---
+++ src/main/java/com/github/unafraid/gradle/multiproject/Main.java
@@ -0,0 +1,12 @@
+package com.github.unafraid.gradle.multiproject;
+
+/**
+ * @author UnAfraid 
+ */
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello gradle!");
+	}
+}
